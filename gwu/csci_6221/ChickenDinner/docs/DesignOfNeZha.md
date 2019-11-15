b68df968485e16d551659bf6807d8d1472a5b0dd
Zheming Gu <zheminggugu@gmail.com>
2019-11-15 02:09:50 -0500

    add nezhe wheel

--- gwu/csci_6221/ChickenDinner/docs/DesignOfNeZha.md
+++ gwu/csci_6221/ChickenDinner/docs/DesignOfNeZha.md
@@ -1,3 +1,80 @@
+# design of Nezha
+## 1.abstract
+There are two forms, one is a large form, the other is a small form, and the initial form is a small form. When a certain condition is reached, it becomes a large form. When a large form reaches a certain condition, it will change back to a small form. The specific conditions will be described in detail later.
+## 2. small form
+### (1) original form
+There are three initial values ​​for small forms: stomach/brain/heart.
+
+1. The value of the stomach is consistent with the normal role, the rate of decrease is lower than the normal role, and the way and value of the increase are consistent with the normal role.
+
+2. The brain value is consistent with the normal role, the rate of decrease is higher than the normal character, and the added mode and value are consistent with the normal role.
+
+3. The heart value is higher than the normal character, only the injury can be reduced. It is consistent with the normal role, and the added mode and value are consistent with the normal role.
+
+4. The moving speed is the same as the normal character.
+
+5. The defense value is the same as the normal role.
+
+### (2) Attack value
+Because all characters in the empty hand state can not attack.
+
+The value is the same as the normal character when the game is set up with tools such as axe/shovel.
+
+### (3)Specific attributes
+1. Fire resistance: normal characters lose their heart values ​​in a fire (such as igniting a forest in a forest), and no one will
+
+2. Resilience: When the heart value of 吒 is less than 5%, it will return to 20%, but only once.
+
+3. On the full moon night (that is, the day the pig is transformed), it will become a big form. All data is carried out according to the big form.
+
+
+## 3, large form
+### (1). Initial value
+1. The value of the stomach is consistent with the normal role, the rate of decrease is higher than the normal character, and the way and value of the increase are consistent with the normal role.
+
+2. The brain value is lower than the normal character, the reduction rate is higher than the normal character, and the added mode and value are consistent with the normal role.
+
+3. The heart value is higher than the normal character, only the injury can be reduced. It is consistent with the normal role, and the added mode and value are consistent with the normal role.
+
+4. The moving speed is higher than the normal character.
+
+5. The defense value is consistent with the normal role.
+
+### (2). Specific attributes
+1. Deceptive: consistent with small form
+
+2. Fire resistance: consistent with small form
+
+3. Resilience: consistent with small form
+
+4. Aggressiveness: The attack value is twice the size of the small form.
+
+5. Brain residual: The brain value decreases with time and the rate of decrease is 3 times that of small form.
+
+## 4, Conversion
+Xiaonao will be transformed into a big one on the full moon night.
+
+or
+
+Xiaonao will transform when the brain value is lower than 60%.
+
+
+## 5, items
+There are three items held in the house: Qiankunquan / Huntianlin / Huojianqiang
+
+## 1.Qiankunquan
+A golden ring, on which body is slanting.
+
+Function: Increase brain data, and increase it by 20%. Normal characters will get brain data recovery through sputum and other methods. Do not wear Qiankun circle, recovery and positive
+The constant value is the same, and it will be restored a little/once. It can't attack.
+
+### 2. Huojianqiang
+Attacking the object at close range, holding it in your hand, the attack power is 5 times that of the hammer. You can't attack the trees, you can only attack the creatures.
+
+### 3.Huntianling
+An armor to enhance defense.
+
+
 # 哪吒的设计
 ## 1、概述
 哪吒分成两个形态，一个是大形态，一个是小形态，初始是以小形态，达到某种条件的时候就会变成大形态。而大形态到达某种条件之后就会变回小形态。具体条件会在后面详细描述。
@@ -81,78 +158,3 @@
 一个防具，提升防御力.
 
 
-# design of Nezha
-## 1.abstract
-There are two forms, one is a large form, the other is a small form, and the initial form is a small form. When a certain condition is reached, it becomes a large form. When a large form reaches a certain condition, it will change back to a small form. The specific conditions will be described in detail later.
-## 2. small form
-### (1) original form
-There are three initial values ​​for small forms: stomach/brain/heart.
-
-1. The value of the stomach is consistent with the normal role, the rate of decrease is lower than the normal role, and the way and value of the increase are consistent with the normal role.
-
-2. The brain value is consistent with the normal role, the rate of decrease is higher than the normal character, and the added mode and value are consistent with the normal role.
-
-3. The heart value is higher than the normal character, only the injury can be reduced. It is consistent with the normal role, and the added mode and value are consistent with the normal role.
-
-4. The moving speed is the same as the normal character.
-
-5. The defense value is the same as the normal role.
-
-### (2) Attack value
-Because all characters in the empty hand state can not attack.
-
-The value is the same as the normal character when the game is set up with tools such as axe/shovel.
-
-### (3)Specific attributes
-1. Fire resistance: normal characters lose their heart values ​​in a fire (such as igniting a forest in a forest), and no one will
-
-2. Resilience: When the heart value of 吒 is less than 5%, it will return to 20%, but only once.
-
-3. On the full moon night (that is, the day the pig is transformed), it will become a big form. All data is carried out according to the big form.
-
-
-## 3, large form
-### (1). Initial value
-1. The value of the stomach is consistent with the normal role, the rate of decrease is higher than the normal character, and the way and value of the increase are consistent with the normal role.
-
-2. The brain value is lower than the normal character, the reduction rate is higher than the normal character, and the added mode and value are consistent with the normal role.
-
-3. The heart value is higher than the normal character, only the injury can be reduced. It is consistent with the normal role, and the added mode and value are consistent with the normal role.
-
-4. The moving speed is higher than the normal character.
-
-5. The defense value is consistent with the normal role.
-
-### (2). Specific attributes
-1. Deceptive: consistent with small form
-
-2. Fire resistance: consistent with small form
-
-3. Resilience: consistent with small form
-
-4. Aggressiveness: The attack value is twice the size of the small form.
-
-5. Brain residual: The brain value decreases with time and the rate of decrease is 3 times that of small form.
-
-## 4, Conversion
-Xiaonao will be transformed into a big one on the full moon night.
-
-or
-
-Xiaonao will transform when the brain value is lower than 60%.
-
-
-## 5, items
-There are three items held in the house: Qiankunquan / Huntianlin / Huojianqiang
-
-## 1.Qiankunquan
-A golden ring, on which body is slanting.
-
-Function: Increase brain data, and increase it by 20%. Normal characters will get brain data recovery through sputum and other methods. Do not wear Qiankun circle, recovery and positive
-The constant value is the same, and it will be restored a little/once. It can't attack.
-
-### 2. Huojianqiang
-Attacking the object at close range, holding it in your hand, the attack power is 5 times that of the hammer. You can't attack the trees, you can only attack the creatures.
-
-### 3.Huntianling
-An armor to enhance defense.
--- gwu/csci_6221/ChickenDinner/scripts/prefabs/nz_wheel.lua
+++ gwu/csci_6221/ChickenDinner/scripts/prefabs/nz_wheel.lua
@@ -1,125 +1,19 @@
 --feng huo lun
-STRINGS.NAMES.NZ_WHEEL = "feng huo lun"
-STRINGS.CHARACTERS.GENERIC.DESCRIBE.NZ_WHEEL = "hiii ha!"
-
-local function GetDesc(inst)
-	inst.components.inspectable.getstatus = function(inst)
-		return inst.state:upper()
-	end
-end
-
 local assets =
 {
 
 	Asset("ANIM","anim/nz_wheel.zip"),
-	-- Asset("ATLAS", "images/inventoryimages/katanas.xml")
+	Asset("ATLAS", "images/inventoryimages/nz_wheel.xml")
 
 }
 
-
-local function setupcontainer(inst, slots, bank, build, inspectslots, inspectbank, inspectbuild, inspectboatbadgepos, inspectboatequiproot)
-	inst:AddComponent("container")
-	inst.components.container:SetNumSlots(#slots)
-	inst.components.container.type = "boat"
-	inst.components.container.side_align_tip = -500
-	inst.components.container.canbeopened = false
-
-	inst.components.container.widgetslotpos = slots
-	inst.components.container.widgetanimbank = bank
-	inst.components.container.widgetanimbuild = build
-	inst.components.container.widgetboatbadgepos = Vector3(0, 40, 0)
-	inst.components.container.widgetequipslotroot = Vector3(-80, 40, 0)
-
-	local boatwidgetinfo = {}
-	boatwidgetinfo.widgetslotpos = inspectslots
-	boatwidgetinfo.widgetanimbank = inspectbank
-	boatwidgetinfo.widgetanimbuild = inspectbuild
-	boatwidgetinfo.widgetboatbadgepos = inspectboatbadgepos
-	boatwidgetinfo.widgetpos = Vector3(200, 0, 0)
-	boatwidgetinfo.widgetequipslotroot = inspectboatequiproot
-	inst.components.container.boatwidgetinfo = boatwidgetinfo
-end
-
-local function OnMounted(inst,data)
-
-	inst.Physics:SetActive(false)
-	--inst.onboatdelta = function () return end
-	
-	if data and data.driver then
-		local driver = data.driver
-
-		driver.components.temperature.hot_resistance_override =  10000
-		driver.components.health.fire_damage_scale = driver.components.health.fire_damage_scale - 1
-		
-		inst.entity:SetParent(driver.entity) 
-		inst.Transform:SetPosition(0,0,0)
-		driver.Physics:ClearCollisionMask()
-    	driver.Physics:CollidesWith(COLLISION.GROUND)
-    	--driver.Physics:CollidesWith(COLLISION.OBSTACLES)
-    	driver.Physics:CollidesWith(COLLISION.CHARACTERS)
-		driver.Physics:CollidesWith(COLLISION.WAVES)
-		driver.components.locomotor:AddSpeedModifier_Mult('nz_wheel',.8)
-		inst:DoTaskInTime(0,function() 
-			inst.Transform:SetPosition(0,0,0)
-			driver.Physics:ClearCollisionMask()
-    		driver.Physics:CollidesWith(COLLISION.GROUND)
-    		--driver.Physics:CollidesWith(COLLISION.OBSTACLES)
-    		driver.Physics:CollidesWith(COLLISION.CHARACTERS)
-			driver.Physics:CollidesWith(COLLISION.WAVES)
-			driver:StopUpdatingComponent(driver.components.driver)
-		end)
-
-		if not inst.pos_task then
-			inst.pos_task = inst:DoPeriodicTask(0,function()
-				if GetPlayer().components.mapwrapper and GetPlayer().components.mapwrapper.state == 3 then
-					inst.Transform:SetPosition(0,0,0)  
-				end
-			end)
-		end
-
-		if not inst.rot_task then
-			inst.rot_task = inst:DoPeriodicTask(0,function()
-				local rot = GetPlayer().Transform:GetRotation()
-				inst.ring1.Transform:SetRotation(rot)
-				inst.ring2.Transform:SetRotation(rot)
-			end)
-		end
-
-	end
-end
-
-local function OnDismounted(inst,data)
-
-	inst.entity:SetParent(nil)
-	inst.Physics:SetActive(true)
-	if data and data.driver then
-
-		data.driver.components.temperature.hot_resistance_override =  nil
-
-		data.driver.components.health.fire_damage_scale = data.driver.components.health.fire_damage_scale + 1
-
-		inst.Transform:SetPosition(data.driver:GetPosition():Get())
-		data.driver.components.locomotor:AddSpeedModifier_Mult('nz_wheel',0)
-	end
-
-	if inst.pos_task then
-		inst.pos_task:Cancel()
-		inst.pos_task = nil
-	end
-	if inst.rot_task then
-		inst.rot_task:Cancel()
-		inst.rot_task = nil
-	end
-
-end
-
 local function OnDropped(inst)
-	inst.entity:SetParent(nil)
-	inst.Physics:SetActive(true)
-	if inst.pos_task then
-		inst.pos_task:Cancel()
-		inst.pos_task = nil
-	end
+	-- inst.entity:SetParent(nil)
+	-- inst.Physics:SetActive(true)
+	-- if inst.pos_task then
+	-- 	inst.pos_task:Cancel()
+	-- 	inst.pos_task = nil
+	-- end
 end
 
 local function ondepletedfn(inst)
@@ -170,7 +64,16 @@ local function onhealthdelta(inst,a,b)
 	end
 end
 
-local function fn()
+local function on_equip(inst,owner)
+	owner.AnimState:ClearOverrideSymbol("swap_body")
+end
+
+local function on_unequip(inst,owner)
+	owner.AnimState:OverrideSymbol("swap_body", "armor_wood", "swap_body")
+end
+
+
+local function init_wheel()
 	print("spawning wheel !!!!")
 	local inst = CreateEntity()
 	inst.entity:AddTransform()
@@ -179,219 +82,30 @@ local function fn()
 	inst.entity:AddFollower()
 	MakeInventoryPhysics(inst)
 
-	inst.Transform:SetEightFaced()
-	inst:DoPeriodicTask(0,function(inst)
-		if not inst.components.drivable.driver then
-			inst.ring1.AnimState:SetScale(1,1)
-			inst.ring2.AnimState:SetScale(1,1)
-			return 
-		end
-		local face = inst.ring1.AnimState:GetCurrentFacing()
-		inst.ring1:SetFaced(face)
-		inst.ring2:SetFaced(face)
-	end)
-
-	inst.AnimState:SetBank("nz_wheel")
     inst.AnimState:SetBuild("nz_wheel")
+    inst.AnimState:PlayAnimation("idle")
     inst.AnimState:SetRayTestOnBB(true)
 
-    local ring1 = SpawnPrefab('nz_wheel_single')
-    inst:AddChild(ring1)
-    ring1.Transform:SetPosition(0,0,0.3)
-    ring1.wheel = inst
-    inst.ring1 = ring1	
-
-    local ring2 = SpawnPrefab('nz_wheel_single')
-    inst:AddChild(ring2)
-    ring2.Transform:SetPosition(0,0,-0.3)
-    ring2.wheel = inst
-    inst.ring2 = ring2
-
-    inst.AnimState:SetFinalOffset(1)
-
-	setupcontainer(inst, {}, "boat_hud_raft", "boat_hud_raft", {}, "boat_inspect_raft", "boat_inspect_raft", {x=0,y=5}, {})
-	inst.components.container.CollectSceneActions = function() return end
-
-	inst:AddComponent('inventoryitem')
-	inst.components.inventoryitem.nobounce = true
-	--inst.components.inventoryitem.atlasname = "images/inventoryimages/mk_cloud.xml"
-	local actionfn = inst.components.inventoryitem.CollectSceneActions
-	inst.components.inventoryitem.CollectSceneActions = function(self,doer,actions,right)
-		if not right then actionfn(self,doer,actions) end
-	end
-
-	inst.SetHot = function(inst)
-		inst.components.inventoryitem.atlasname = "images/inventoryimages/nz_wheel_hot.xml"
-		inst.components.inventoryitem:ChangeImageName("nz_wheel_hot")
-		inst.state = 'HOT'
-		inst.ring1:Show()
-		inst.ring2:Show()
-
-		if inst.components.floatable then
-			inst.components.floatable.wateranim = 'hot'
-			inst.components.floatable.land_anim = 'hot'
-			inst.components.floatable:UpdateAnimations()
-		end
-		inst.AnimState:SetMultColour(0,0,0,0)  ---   _(:з」∠)_
-	end
-
-	inst.SetCold = function(inst)
-		inst.components.inventoryitem.atlasname = "images/inventoryimages/nz_wheel_cold.xml"
-		inst.components.inventoryitem:ChangeImageName("nz_wheel_cold")
-		inst.ring1:Hide()
-		inst.ring2:Hide()
-		inst.ring1:KillFireFx()
-		inst.ring2:KillFireFx()
-		inst.state = 'COLD'
-
-		if inst.components.floatable then
-			inst.components.floatable.wateranim = 'cold_water'
-			inst.components.floatable.land_anim = 'cold'
-			inst.components.floatable:UpdateAnimations()
-		end
-		inst.AnimState:SetMultColour(1,1,1,1)
-	end
-
-	local fueltime =  3*480  
-	local rate = 100/480 
-	local deltap = 1/fueltime  
-	inst:AddComponent('fueled')
-	inst.components.fueled.maxfuel = 3*100
-	inst.components.fueled:InitializeFuelLevel(0)
-	inst.components.fueled.rate = rate
-	inst.components.fueled:SetDepletedFn(ondepletedfn)
-	inst.components.fueled.fueltype = "NZ_WHEEL"
-	inst.components.fueled.OnSave = function(self) return {fuel = self.currentfuel} end
-	--inst.components.fueled.accepting = false
-	--inst.components.fueled:SetPercent(0)
-	inst.components.fueled.CanAcceptFuelItem = function(self, item)
-		return inst.state == 'HOT' and item and item.prefab == 'redgem'
-	end
-	--inst.components.fueled:StartConsuming()
-
-	inst:ListenForEvent('percentusedchange',function(inst) 
-		local now = inst.components.fueled:GetPercent()
-		inst:PushEvent('cloudhealthchange',{percent=now,oldpercent=now+deltap,maxhealth=inst.components.fueled.maxfuel,});
-		onhealthdelta(inst,now,now+deltap)
-	end)
-	
-	
-	inst:AddComponent("drivable")
-	inst.components.drivable.overridebuild = 'mk_cloudfx'
-	inst.components.drivable.flotsambuild = 'mk_cloudfx'
-
-	inst.components.drivable.prerunanimation = 'surf_pre'
-	inst.components.drivable.runanimation = 'surf_loop'
-	inst.components.drivable.postrunanimation = 'surf_pst'
-
-	inst.components.drivable.sailloopanim = "sail_loop"
-	inst.components.drivable.sailstartanim = "sail_pre"
-	inst.components.drivable.sailstopanim = "sail_pst"
-
-	inst.components.drivable.alwayssail = true
-	inst.components.drivable.hitfx = 'mk_firefire2'
-
-	inst.components.drivable.creaksound = ''
-	inst.components.drivable.runspeed = .5
-	inst.components.drivable.donothideonmounted = true
-	inst.components.drivable.maprevealbonus = 3
-	inst.components.drivable.candrivefn = function(inst) return inst.state == 'HOT' end
-
-	inst:ListenForEvent('mounted',OnMounted)
-	inst:ListenForEvent('dismounted',OnDismounted)
-	inst:ListenForEvent('ondropped',OnDropped)
-
-	inst:AddComponent('inspectable')
-
-	inst:AddTag('irreplaceable')
-
-	inst:SetCold()
-	--inst:SetHot()
-	inst.OnLoad = function(inst,data)
-		if inst.components.fueled:IsEmpty() then
-			inst:SetCold()
-		else
-			inst:SetHot()
-		end
-	end
-	inst:DoTaskInTime(0,function()
-		if GetPlayer().prefab ~= 'neza' then
-			inst:Remove() --@--
-		end
-	end)
-	GetDesc(inst)
-
-	return inst
-end
-
-local function fn2()
-	local inst = CreateEntity()
-	inst.entity:AddTransform()
-    inst.entity:AddAnimState()
-	inst.entity:AddSoundEmitter()
-
-	inst.entity:AddPhysics()
-
-	inst.AnimState:SetBank("nz_wheel")
-    inst.AnimState:SetBuild("nz_wheel")
-    inst.AnimState:Hide('center1');inst.AnimState:Hide('center2');inst.AnimState:Hide('center3')
-    inst.AnimState:PlayAnimation('flying',true)
-    inst.AnimState:SetRayTestOnBB(true)
-
-
-    inst.SpawnFirefx = function (inst)
-    	if inst.wheel and inst.wheel.state == 'COLD' then
-    		return
-    	end
-    	if not inst.firefx then
-    		local fire = SpawnPrefab('torchfire')
-    		fire.entity:AddFollower()
-    		fire.entity:MoveToFront()
-    		fire.Follower:FollowSymbol(inst.GUID,'center3',0,0,0)
-    		inst.firefx = fire
-    	end
-    end
-
-    inst.KillFireFx = function(inst)
-    	if inst.firefx then
-    		inst.firefx:Remove()
-    		inst.firefx = nil
-    	end
-    end
-
-    inst:DoTaskInTime(0,function(inst)
-    	if not (inst.wheel and inst.wheel:HasTag("INLIMBO"))then
-    		inst:SpawnFirefx()
-    	end
-    	inst:ListenForEvent('enterlimbo',function(wheel)inst:KillFireFx()end,inst.wheel)
-		inst:ListenForEvent('exitlimbo',function(wheel)inst:SpawnFirefx()end,inst.wheel)
-    end)
-    
-    inst.AnimState:SetFinalOffset(-5)
-
-	inst.Transform:SetScale(1.2, 1.2, 1.2)
 	inst.Transform:SetEightFaced()
-	inst.SetFaced = function(inst,face) 
-		if face == FACING_LEFT or face == FACING_RIGHT then
-			inst.AnimState:SetScale(1,1)
-		elseif face == FACING_UP or face == FACING_DOWN then
-			inst.AnimState:SetScale(.2,1)
-		else
-			inst.AnimState:SetScale(.5,1)
-		end
-	end
-	inst:AddComponent('inspectable')
-	inst.components.inspectable.onlyforcedinspect = true
-	inst.components.inspectable.CollectSceneActions = function(self,doer, actions, right) 
-    	if doer.components.driver and right and inst.wheel and inst.wheel.state == 'HOT' then
-           	table.insert(actions, ACTIONS.MOUNT)
-    	end
-	end
+	inst:AddTag("irreplaceable")
+	inst:AddComponent("inspectable")
+	
+    inst:AddComponent("inventoryitem")
+    inst.components.inventoryitem.atlasname = "images/inventoryimages/nz_wheel.xml"
+	inst.components.inventoryitem:SetOnDroppedFn(OnDropped)
+		
 
-	GetDesc(inst)
+	inst:AddComponent("armor")
+    inst.components.armor:InitCondition(TUNING.ARMORWOOD, TUNING.ARMORWOOD_ABSORPTION)
+
+    inst:AddComponent("equippable")
+    inst.components.equippable.equipslot = EQUIPSLOTS.BODY
+    inst.components.equippable:SetOnEquip( on_equip )
+	inst.components.equippable:SetOnUnequip( on_unequip )
 
 	return inst
 end
 
 
-return Prefab("nz_wheel", fn, assets),Prefab("nz_wheel_single", fn2, assets)
+
+return Prefab("nz_wheel", init_wheel, assets)
